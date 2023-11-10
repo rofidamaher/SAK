f0680ebd710e6da6620ff0122c3238857ef5dcab
AbdullahJava <abahr@sirius-its.com>
2023-11-10 16:10:11 +0200

    MORTGAGE_LAND

---
+++ src/test/java/org/example/pages/First_Step/P09_MORTGAGE.java
@@ -0,0 +1,18 @@
+package org.example.pages.First_Step;
+
+import org.example.stepDefs.Hooks;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class P09_MORTGAGE {
+
+    public WebElement MORTGAGE_LAND()
+    {
+        return Hooks.driver.findElement(By.xpath("//img[@ng-src='img/MORTGAGE_LAND.png']"));
+    }
+
+    public WebElement MORTGAGE_label()
+    {
+        return Hooks.driver.findElement(By.id("referrer"));
+    }
+}
